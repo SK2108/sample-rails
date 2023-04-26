47cf5f545405f669ad78b42fe2c2d1e9ff0d8c88
SK2108 <85369151+SK2108@users.noreply.github.com>
2023-04-26 15:01:20 +0200

    Updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  root to: "static_pages#home"
+
+  # get "static_pages/home"
+
+  # get "static_pages/help"
+  get "/help", to: "static_pages#help"
+
+  # get "static_pages/about"
+  get "/about", to: "static_pages#about"
+
+  # get "static_pages/contact"
+  get "/contact", to: "static_pages#contact"
+
+  get "/login", to: "session#new"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
