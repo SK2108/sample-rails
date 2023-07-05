28050ea63fe1f4c723d72cd2ed09a57ea8aeda5d
SK2108 <85369151+SK2108@users.noreply.github.com>
2023-07-05 13:08:29 +0200

    Adding a comment to both controllers to fix the error in Rubocop

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,13 @@
+# frozen_string_literal: true
+
+# This is a Static page controller
 class StaticPagesController < ApplicationController
   # the new actions we have created are called 'home' and 'help'
-  def home
-  end
+  def home; end
 
-  def help
-  end
+  def help; end
 
-  def about
-  end
+  def about; end
 
-  def contact
-  end
+  def contact; end
 end
